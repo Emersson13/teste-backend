29718a2ac63b351c6bd36088ec5d05d462048bee
Bruno Costa <analistabrunocosta@gmail.com>
2022-10-19 20:53:31 -0300

    Continuando a trabalhar com phpunit

--- aula3/test/SlugTest.php
+++ aula3/test/SlugTest.php
@@ -3,5 +3,10 @@ use PHPUnit\Framework\TestCase;
 Use App\Slug;
 
 class SlugTest extends TestCase{
-    
+   
+    public function test_converter(){
+        $meuSlug = new Slug("  Programacao PHP  ");
+
+        $this->assertEquals($meuSlug->converter(), "programacao-php");
+    }
 }
