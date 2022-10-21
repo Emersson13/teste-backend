97f55d8c3b0f5c8ea67d398bbd669623dd7463ad
Emersson <emerssonstaind15@gmail.com>
2022-10-21 19:20:44 -0300

    Finalizado testes back-end

--- aula4/test/ClienteTest.php
+++ aula4/test/ClienteTest.php
@@ -4,4 +4,23 @@ use App\Cliente;
 
 class ClientTest extends TestCase{
     
+    public function testVerificarCamposVazio(){
+        $meuCliente = new Cliente("Zilda", "zilda@email.com");
+        
+        $this->assertFalse($meuCliente->validaCampos());
+    }
+
+    public function testContarItensPreferencia(){
+        $meuCliente = new Cliente("Laele","laele@laele.com",456);
+        $meuCliente->setPreferencias(["Promoção", "Roupa Barata","Comida Baiana"]);
+
+        $this->assertCount(3,$meuCliente->getPreferencias());// Esse teste irá contar quantos itens existem dentro do vetor
+    }
+
+    public function testVerificarTotalCompras(){
+        $meuCliente = new Cliente("Junior", "junior@email.com",123456);
+        $meuCliente->setTotalCompras(450);
+
+        $this->assertGreaterThan(400, $meuCliente->getTotalCompras());
+    }
 }
